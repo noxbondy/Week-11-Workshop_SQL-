cfa7187824973bfd8587af4c6f41737f0d93db65
Md gulam Noxboundy <gulamnoxbondy@gmail.com>
2025-03-12 22:28:14 +0100

    Add files via upload

--- sql_2_join.sql
+++ sql_2_join.sql
@@ -78,79 +78,72 @@ HAVING COUNT(*) > 200
 ORDER BY city_count DESC;
 
 # 13: What language(s) is spoken in the city with a population between 400 and 500 ?
+SELECT cl.Language, ci.name, ci.Population as CityName
+FROM city ci
+         LEFT JOIN countrylanguage cl ON ci.CountryCode = cl.CountryCode
+WHERE population BETWEEN 400 AND 500;
 
-SELECT c.Name AS City, cl.Language
-FROM city c
-JOIN countrylanguage cl ON c.CountryCode = cl.CountryCode
-WHERE c.Population BETWEEN 400 AND 500;
 
 # 14: What are the name(s) of the cities with a population between 500 and 600 people and the language(s) spoken in them
 
-SELECT c.city_name, l.language
-FROM City c
-JOIN Languages l ON c.city_name = l.city_name
-WHERE c.population BETWEEN 500 AND 600;
+SELECT ci.name, cl.language
+FROM city ci
+         JOIN countrylanguage cl ON ci.CountryCode = cl.CountryCode
+WHERE ci.population BETWEEN 500 AND 600;
 
 # 15: What names of the cities are in the same country as the city with a population of 122199 (including the that city itself)
-
-SELECT name
-FROM city
-WHERE ID = (
-    SELECT ID
-    FROM city
-    WHERE Population = 122199
-)
-ORDER BY name;
+SELECT ci.name as CityName, ci.countrycode, ci.population
+FROM city ci
+WHERE ci.CountryCode = (SELECT CountryCode FROM city WHERE population = 122199);
+#Using LEFT JOIN
+SELECT ci1.name, ci1.countrycode, ci1.population
+FROM city ci2
+         LEFT JOIN city ci1 ON ci2.CountryCode = ci1.CountryCode
+WHERE ci2.Population = 122199;
 
 # 16: What names of the cities are in the same country as the city with a population of 122199 (excluding the that city itself)
 
-SELECT name
-FROM city
-WHERE name = (
-    SELECT name
-    FROM city
-    WHERE Population = 122199
-    LIMIT 1
-)
-AND Population != 122199;
+SELECT ci.name as CityName, ci.population
+FROM city ci
+WHERE ci.CountryCode = (SELECT CountryCode FROM city WHERE population = 122199)
+  AND ci.population != 122199;
+
+
 
 # 17: What are the city names in the country where Luanda is capital?
 
-SELECT name
-FROM City c
-JOIN Countries co ON c.country_ID = co.country_ID
-WHERE co.capital = 'Luanda';
+SELECT ci.name AS CityName, ci.CountryCode
+from city ci
+         JOIN country co ON ci.CountryCode = co.Code
+WHERE co.Capital = (SELECT ID from city where name = 'Luanda');
+
+SELECT ci.name AS CityName, ci.CountryCode
+from city ci
+         JOIN country co ON ci.CountryCode = co.Code
+WHERE co.Capital = (SELECT ID from city where name = 'Luanda')
+  AND ci.name != 'Luanda';
 
 # 18: What are the names of the capital cities in countries in the same region as the city named Yaren
 
-SELECT c1.city_name AS CapitalCity
-FROM city c1
-JOIN countries co1 ON c1.country_id = co1.country_id
-JOIN regions r1 ON co1.region_id = r1.region_id
-WHERE c1.city_name IN (SELECT c2.city_name
-                        FROM city c2
-                        JOIN countries co2 ON c2.country_id = co2.country_id
-                        JOIN regions r2 ON co2.region_id = r2.region_id
-                        WHERE c2.city_name = 'Yaren')
-AND c1.city_name <> 'Yaren';
+SELECT ci.name AS CapitalName
+from country co
+         JOIN city ci ON ci.ID = co.capital
+WHERE co.Region = (SELECT co.Region
+                   FROM country co
+                            JOIN city ci ON ci.ID = co.Capital
+                   WHERE ci.name = 'Yaren');
 
 # 19: What unique languages are spoken in the countries in the same region as the city named Riga
 
-SELECT DISTINCT l.language
-FROM languages l
-JOIN country c ON l.country_id = c.id
-JOIN regions r ON c.region_id = r.id
-WHERE r.name = (
-    SELECT r.Name
-    FROM country c
-    JOIN regions r ON c.region_id = r.id
-    WHERE c.Name = 'Latvia'
-);
+SELECT DISTINCT cl.Language
+FROM city ci
+         JOIN country co ON ci.CountryCode = co.Code
+         JOIN countrylanguage cl ON co.code = cl.CountryCode
+WHERE co.Region = (SELECT Region FROM country WHERE Code = (SELECT CountryCode FROM city WHERE name = 'Riga'));
 
 # 20: Get the name of the most populous city
 
-SELECT name
+SELECT name, population
 FROM city
-ORDER BY Population DESC
-LIMIT 1;
+WHERE population = (SELECT MAX(population) FROM city);
 
